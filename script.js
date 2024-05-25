389dd7b180560980fbe22a050d7151f23985e443
Jimena Segura <jimensitica@gmail.com>
2024-05-24 22:07:52 -0300

    arreglo footer, nav, mq pág carrito, contacto

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ function changeValue(delta) {
         input.value = newValue;
     }
 }
+
+function scrollLeftContent() {
+    const scrollContainer = document.querySelector('.scroll-content');
+    scrollContainer.scrollBy({
+        left: -200, // Ajusta este valor según la cantidad de desplazamiento deseado
+        behavior: 'smooth'
+    });
+}
+
+function scrollRight() {
+    const scrollContainer = document.querySelector('.scroll-content');
+    scrollContainer.scrollBy({
+        left: 200, // Ajusta este valor según la cantidad de desplazamiento deseado
+        behavior: 'smooth'
+    });
+}
+function scrollLeftContent2() {
+    const scrollContainer2 = document.querySelector('.scroll-content2');
+    scrollContainer2.scrollBy({
+        left: -200, // Ajusta este valor según la cantidad de desplazamiento deseado
+        behavior: 'smooth'
+    });
+}
+
+function scrollRight2() {
+    const scrollContainer2 = document.querySelector('.scroll-content2');
+    scrollContainer2.scrollBy({
+        left: 200, // Ajusta este valor según la cantidad de desplazamiento deseado
+        behavior: 'smooth'
+    });
+}
